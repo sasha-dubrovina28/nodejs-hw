c010091b5bf3a16289baacefce510f33ad10b813
Oleksandra Dubrovina <sashaaagfg4@gmail.com>
2026-08-25 12:29:59 +0300

    add

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,10 +2,46 @@ import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
+  const {
+    page = 1,
+    perPage = 10,
+    tag,
+    search,
+  } = req.query;
 
-  res.status(200).json(notes);
+  const skip = (page - 1) * perPage;
+
+  const notesQuery = Note.find();
+
+  if (tag) {
+    notesQuery.where({ tag });
+  }
+
+  if (search) {
+  notesQuery.where({
+    $or: [
+      { title: { $regex: search, $options: 'i' } },
+      { content: { $regex: search, $options: 'i' } },
+    ],
+  });
+}
+
+  const [totalNotes, notes] = await Promise.all([
+    notesQuery.clone().countDocuments(),
+    notesQuery.skip(skip).limit(perPage),
+  ]);
+
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalNotes,
+    totalPages,
+    notes,
+  });
 };
+
 
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
---
+++ src/validations/notesValidation.js
@@ -0,0 +1,45 @@
+import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
+
+
+const noteIdValidator = (value, helpers) => {
+  if (!isValidObjectId(value)) {
+    return helpers.error('any.invalid');
+  }
+
+  return value;
+};
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    search: Joi.string().trim().allow("").optional(),
+  }),
+};
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(noteIdValidator).required(),
+  }),
+};
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow("").optional(),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .optional(),
+  }),
+};
+export const updateNoteSchema = {
+  [Segments.PARAMS]: noteIdSchema[Segments.PARAMS],
+
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).optional(),
+    content: Joi.string().allow('').optional(),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .optional(),
+  }).min(1),
+};
